88faefe308f0a63df95db9aad96625ceaf7f712f
kiritomoreno <kiritomoreno@MORENO>
2023-07-03 19:13:13 +0100

    instalando fastapi y uvicorn['standard']-Retornamos un html- Web server con FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+# Instancia
+app = FastAPI()
 
+
+@app.get('/') # Decorador- La ruta donde vamos a ingresar
+def get_list():
+    return [1,2,3]
+
+
+@app.get('/contact',response_class=HTMLResponse) # Decorador- La ruta donde vamos a ingresar
+def get_list():
+    return'''
+    <h1>Hola soy una pagina</h1>
+    <p>Soy un parrafo</p>
+    '''
+#  uvicorn main:app --reload (comando para usar en la terminal)
 def run():
     store.get_categories()
 
